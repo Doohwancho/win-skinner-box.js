cbcd3d8b5f4333e3a88e6c0196dc67b7379d886a
Doohwancho <doohwancho526@gmail.com>
2022-07-20 22:41:39 +0000

    refactor: DOMNodeInserted -> MutationObserver

--- src/js/content.js
+++ src/js/content.js
@@ -1,12 +1,28 @@
 
-const frame = document.querySelector('#content');
-console.log(frame);
+const targetNode = document.querySelector('#content');
 
-frame.addEventListener( 'DOMNodeInserted', function ( event ) {
-    if( event.target.id == 'title' ) { 
-        if(event.target.innerText === 'Today'){
-            event.target.parentNode.parentNode.parentNode.parentNode.remove();
+const config = { attributes: true, childList: true, subtree: true, characterDataOldValue: true};
+
+const callback = function(mutationList, observer) {
+    for(const mutation of mutationList) {
+        for(let node of mutation.addedNodes) {
+            if (!(node instanceof HTMLElement)) continue;
+            
+            if (node.matches('[id*="dismissible"]')) { 
+                if(node.className == 'style-scope ytd-shelf-renderer'){
+                    if(node.querySelector('#title').innerText == 'Today'){
+                        node.hidden = true;
+                    }
+                }
+            }
         }
-    };
-}, false );
+    }
+};
+
+const observer = new MutationObserver(callback);
+
+observer.observe(targetNode, config);
+
+// Later, you can stop observing
+// observer.disconnect();
 
