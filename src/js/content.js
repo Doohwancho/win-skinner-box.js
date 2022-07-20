00bedf3e18b74bd10f37b9c281d5b9acaeafcdb9
Doohwancho <doohwancho526@gmail.com>
2022-07-20 15:43:05 +0000

    reorganize project structure && hide youtube today's subscription videos

---
+++ src/js/content.js
@@ -0,0 +1,4 @@
+
+const target = document.querySelector('.style-scope.ytd-section-list-renderer:nth-child(1) > DIV.style-scope.ytd-item-section-renderer:nth-child(3)');
+target.parentElement.removeChild(target);
+
