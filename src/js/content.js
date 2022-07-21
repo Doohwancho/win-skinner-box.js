4d01d54139f39f819d7da2b248280303629f2454
Doohwancho <doohwancho526@gmail.com>
2022-07-21 16:18:10 +0000

    bugfix: youtube.com/feed/subscription 첫 접속시 Today 차단 안되는 문제 해결

--- src/js/content.js
+++ src/js/content.js
@@ -1,18 +1,19 @@
-
 const targetNode = document.querySelector('#content');
 
-const config = { attributes: true, childList: true, subtree: true, characterDataOldValue: true};
+const config = { childList: true, subtree: true};
 
 const callback = function(mutationList, observer) {
     for(const mutation of mutationList) {
+
         for(let node of mutation.addedNodes) {
             if (!(node instanceof HTMLElement)) continue;
             
-            if (node.matches('[id*="dismissible"]')) { 
+            if (node.matches('[id*="dismissible"]')) {
                 if(node.className == 'style-scope ytd-shelf-renderer'){
                     if(node.querySelector('#title').innerText == 'Today'){
                         node.hidden = true;
-                    }
+                        node.parentNode.parentNode.hidden = true;
+                    } 
                 }
             }
         }
@@ -22,7 +23,4 @@ const callback = function(mutationList, observer) {
 const observer = new MutationObserver(callback);
 
 observer.observe(targetNode, config);
-
-// Later, you can stop observing
-// observer.disconnect();
 
